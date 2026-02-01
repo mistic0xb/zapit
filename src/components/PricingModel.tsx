eb13ea644a57cb7575e1df06f12213e74901e0fd
Mist <mistic0xb@gmail.com>
2026-02-01 21:50:17 +0530

    changed pricing

--- src/components/PricingModel.tsx
+++ src/components/PricingModel.tsx
@@ -145,7 +145,7 @@ function Pricing() {
                 </p>
                 <div className="flex items-baseline gap-2 mb-2">
                   <span className="text-4xl sm:text-5xl md:text-6xl proj:text-8xl font-bold text-yellow-text">
-                    1.1K
+                    2.1K
                   </span>
                   <span className="text-base sm:text-lg md:text-xl proj:text-3xl text-gray-400">
                     sats
